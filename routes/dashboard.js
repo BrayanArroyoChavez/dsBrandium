faeadadd30356e5e9344994e982ca7d84304db36
BrayanArroyoChavez <brayan.arroyo.chavez@gmail.com>
2021-04-26 12:04:38 -0500

    se agrego la conexión a la base de datos a un archivo externo a las rutas

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,23 +1,7 @@
 const { Router } = require('express');
 const router = new Router();
 
-var mysql      = require('mysql');
-
-var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : '',
-  database : 'kampsys'
-});
-
-connection.connect(function(err) {
-    if (err) {
-      console.error('error connecting: ' + err.stack);
-      return;
-    }
-   
-    console.log('connected as id ' + connection.threadId);
-  });
+const connection = require('../connection')
 
 router.get('/dashboard',async (req,res) =>{
     const marcas = await connection.query('SELECT * FROM `marcas_renovacion` where id < 7000', function (error, results, fields) {
