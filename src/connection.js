51c7942398d1123e3112843b2df6dbec6a0e131d
brayanarroyochavez <brayan.arroyo.chavez@gmail.com>
2021-05-03 10:39:09 -0500

    Se cambio la conexión a la base de datos por un host gratuito en clever cloud

--- src/connection.js
+++ src/connection.js
@@ -2,8 +2,8 @@ const Sequelize = require('sequelize');
 
 const marcas_renovacionModel = require('../models/marcas_renovacion');
 
-const sequelize = new Sequelize('kampsys','root','',{
-  host     : 'localhost',
+const sequelize = new Sequelize('b5ieyox0oo6iha0jf6t8','u0u3tbtirq65ttqn','gVp3XMYOqwj6a6kvOpN5',{
+  host     : 'b5ieyox0oo6iha0jf6t8-mysql.services.clever-cloud.com',
   dialect  : 'mysql'
 })
 
