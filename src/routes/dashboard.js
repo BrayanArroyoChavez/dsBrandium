aae060b8f5719678d78f1da4809f5f52a0fd8037
brayanarroyochavez <brayan.arroyo.chavez@gmail.com>
2021-05-04 12:01:32 -0500

    Se añadieron comentarios al archivo de rutas del dashboard

--- src/routes/dashboard.js
+++ src/routes/dashboard.js
@@ -1,42 +1,61 @@
+//Creación de rutas
 const { Router } = require('express');
 const router = new Router();
+
+//Se importa la libreria Sequelize y Op que contiene los operadores que se pueden emplear en las consultas a las base de datos
 const { Op, Sequelize } = require('sequelize');
 
+//Se importa el modelo de la tabla de marcas_renovacion
 const { marcas_renovacion } = require('../connection')
+
+//Se importan las funciones contenidas en el archivo dashboard.js 
 const fn = require('../public/js/dashboard.js')
 
+//Ruta para mostrar el dashboard
 router.get('/dashboard',async (req,res) =>{
-  const page = parseInt(req.query.Page) || 1;
-  const limit = parseInt(req.query.Size) || 50;
-  
-  let offset = (limit*page) - limit;
+  //Se derminan las condiciones de filtrado en caso de que se hayan aplicado
   const where = fn.getCondicion(req,Op);
+  //Se determian las opciones de filtrado seleccionadas para marcarlas como seleccionadas en la página
   const filter = fn.getFiltro(req);
   
-  if (where.length != 0){
+  //Dado que la función getCondicion devuelve un arreglo se valida si es diferente de 0 para determinar si hay condiciones de filtrado
+  if (where.length != 0){ 
+    //Función para extraer de la base de datos las marcas registradas con las condiciones determinadas
     marcas = await marcas_renovacion.findAndCountAll({
       where: where
     }).then(result => {
       return result
     });
   }else{
+    //Función para extraer de la base de datos las marcas registradas
     marcas = await marcas_renovacion.findAndCountAll({
     }).then(result => {
       return result
     });
   }
   
+  //Función para extraer de la base de datos los distintos valores que tiene el campo que registra la situación de la marca
+  //El resultado se muestra como una de las opciones a elegir en los filtros del dashboard dentro de un dropdown
   situacion = await marcas_renovacion.findAll({
     attributes: [Sequelize.fn('DISTINCT', Sequelize.col('situacion_marca')), 'situacion_marca'] 
   });
+
+  //Función para extraer de la base de datos los distintos valores que tiene el campo que registra la clase de la marca
+  //El resultado se muestra como una de las opciones a elegir en los filtros del dashboard dentro de un dropdown
+  //Pendiente de modificación para que muestre valores unicos
   clase = await marcas_renovacion.findAll({
     attributes: [Sequelize.fn('DISTINCT', Sequelize.col('clase_marca')), 'clase_marca']
   });
+
+  //Función para extraer de la base de datos los distintos valores que tiene el campo que registra la clasificación de la marca
+  //El resultado se muestra como una de las opciones a elegir en los filtros del dashboard dentro de un dropdown
   clasificacion = await marcas_renovacion.findAll({
     attributes: [Sequelize.fn('DISTINCT', Sequelize.col('clasificacion_niza')), 'clasificacion_niza']
   });
-  res.render('dashboard.ejs', {marcas: marcas.rows, situacion: situacion, clase: clase, clasificacion: clasificacion, 
-                              page: page, lastpage: Math.ceil(marcas.count/limit), size: limit, count: marcas.count, filter: filter });
+
+  //Se renderiza en la página el archivo dashboard.ejs en la ruta /dashboard y se le envian las variables determinadas en el arreglo
+  res.render('dashboard.ejs', {marcas: marcas.rows, situacion: situacion, clase: clase, clasificacion: clasificacion, filter: filter });
  });
 
+ //Se exporta las rutas definidas
  module.exports = router
