1f49ebe864e390f30c0d1bce8dc42d6e22e33ac9
brayanarroyochavez <brayan.arroyo.chavez@gmail.com>
2021-05-04 10:53:41 -0500

    Se añadieron comentarios al archivo donde se realizan la conexión de la base de datos-connection.js

--- src/connection.js
+++ src/connection.js
@@ -1,19 +1,31 @@
+//Se importa sequelize el cual se utiliza para la conexión a la base de datos y a su vez para la creación de modelos 
 const Sequelize = require('sequelize');
 
+//Se importa el modelo de la tabla marcas_renovacion de la base de datos
 const marcas_renovacionModel = require('../models/marcas_renovacion');
 
+//Conexión a la base de datos
+/**
+ * Sequelize(Nombre de la base de datos, usuario, contraseña, {
+    host     : host de la base de datos,
+    dialect  : sistema gestor de la base de datos
+})
+ */
 const sequelize = new Sequelize('b5ieyox0oo6iha0jf6t8','u0u3tbtirq65ttqn','gVp3XMYOqwj6a6kvOpN5',{
   host     : 'b5ieyox0oo6iha0jf6t8-mysql.services.clever-cloud.com',
   dialect  : 'mysql'
 })
 
+//Creación del modelo de la tabla marcas_renovacion
 const marcas_renovacion = marcas_renovacionModel(sequelize,Sequelize);
 
+//Crea las tablas de la base de datos en caso de que no exitan
 sequelize.sync({ force:false })
   .then(() =>{
     console.log("Tablas sincronizadas")
 })
 
+//Exportación del modelo la tabla marcas_renovacion y la conexión a la base de datos
 module.exports = {
   marcas_renovacion,
   sequelize
