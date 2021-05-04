25d6b4cb21255b6d86fac0012c807896855fb1e2
brayanarroyochavez <brayan.arroyo.chavez@gmail.com>
2021-05-04 10:32:44 -0500

    Se añadieron comentarios al archivo de arranque-index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 //Dependencias 
 const express = require('express');
 const morgan = require('morgan');
-const bodyParser = require('body-parser')
 const app = express();
 
-//Configuración
+//Conexión a la base de datos
 require('./connection')
 
+//Configuración
 app.set('port', process.env.PORT || 3000);
 app.set('view engine', 'ejs');
 app.set('views',__dirname + '/views');
@@ -22,7 +22,7 @@ app.use(morgan('dev'));
 //Rutas
 app.use(require('./routes/dashboard'));
 
-//Conexión
+//Conexión al servidor
 app.listen(app.get('port'), () =>{
     console.log('servidor en el puerto', app.get('port'))
 })
